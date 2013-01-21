2c6a539b37535ab091e838b2ccb4275a5b8a8ca7
Patrik Jira <patrikjira@gmail.com>
2013-01-21 10:23:34 +0100

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,13 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.11'
+gem 'sqlite3'
+gem 'jquery-rails'
+gem 'forem', :git => "git://github.com/radar/forem.git"
+gem 'kaminari', '0.13.0'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsForum::Application.config.secret_token = '49e86451f30332e87cf0edcb20a9d82454bebd192e7f4004507b5bad33e17b14dcb7b47128192955bab35a648e531e2f3324bd7d89c6ff227607b3f0f9eeea81'
